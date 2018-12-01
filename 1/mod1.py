f241e0fb1aaaa464ab70d4570cde89227a5a5457
Nic McElroy <nicamcelroy@gmail.com>
2018-12-01 18:48:23 +0000

    add type hints

--- 1/mod1.py
+++ 1/mod1.py
@@ -1,14 +1,22 @@
 from copy import copy
+from typing import List, Generator
 
 
-def get_offsets(starting_offset, instructions):
+def get_offsets(
+    starting_offset: int,
+    instructions: List[int]
+) -> Generator[int, None, None]:
     next_offset = copy(starting_offset)
     for i in instructions:
         next_offset += i
         yield next_offset
 
 
-def get_first_repeat(starting_offset, instructions, seen=set()):
+def get_first_repeat(
+    starting_offset: int,
+    instructions: List[int],
+    seen: set=set()
+) -> int:
     for i in get_offsets(starting_offset, instructions):
         if i in seen:
             return i
